12b3a0c0b50d0d5afbc676b6c084dd34698b564b
omnia <omniacis1@gmail.com>
2025-06-23 16:22:22 +0300

    feat: add sucess payment

---
+++ lib/features/payment_sucess/presentation/view/sucess_payment.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:medical/core/constant_text.dart';
+import 'package:medical/core/utils/assets.dart';
+import 'package:medical/core/utils/styles.dart';
+import 'package:medical/core/widgets/custom_elevated_button.dart';
+import 'package:sizer/sizer.dart';
+
+class SucessPayment extends StatelessWidget {
+  const SucessPayment({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 5.w, vertical: 2.h),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 20.h),
+              Image.asset(AssetsData.sucess),
+              SizedBox(height: 5.h),
+              Text(ConstantText.thankYou, style: Styles.textStyle24),
+              SizedBox(
+                width: 70.w,
+                child: Center(
+                  child: Text(
+                    "Your Order will be delivered with invoice #9ds69hs. You can track the delivery in the order section.",
+                    style: Styles.textStyle14,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              SizedBox(height: 25.h),
+              CustomElevatedButton(
+                buttonText: ConstantText.continueOrder,
+                onPressedFunction: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
