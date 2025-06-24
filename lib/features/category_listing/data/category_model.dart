5a81c92e17b3b0c24fbc316a616e11fd5ee541ce
omnia <omniacis1@gmail.com>
2025-06-24 17:07:12 +0300

    feat: add category listing screen and product item widget

---
+++ lib/features/category_listing/data/category_model.dart
@@ -0,0 +1,10 @@
+class CategoryModel {
+  final String title;
+  final String imageUrl;
+  final String description;
+  CategoryModel({
+    required this.title,
+    required this.imageUrl,
+    required this.description,
+  });
+}
