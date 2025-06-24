fd09cf716e3bd8df4a6e2f8c6e62706dd9c4fb31
omnia <omniacis1@gmail.com>
2025-06-24 17:05:46 +0300

    add image for category

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -5,6 +5,7 @@ class AssetsData {
   static const String addCart = "assets/images/addCart.svg";
   static const String medicine = "assets/images/medicine.png";
   static const String medicine2 = "assets/images/medicine2.png";
+  static const String medicine3 = "assets/images/medicine3.png";
   static const String payment = "assets/images/payment.png";
   static const String sucess = "assets/images/sucess.png";
   static const String cart = "assets/images/cart.png";
@@ -14,6 +15,7 @@ class AssetsData {
   static const String shopping = "assets/images/btn_shopping_cart.svg";
   static const String saveHome = "assets/images/savehome.png";
   static const String productImage = "assets/images/productImage.png";
+  static const String productImage1 = "assets/images/product_image.png";
   static const String home1 = "assets/images/home.png";
   static const String user = "assets/images/user.png";
   static const String shopping1 = "assets/images/shipping.png";
