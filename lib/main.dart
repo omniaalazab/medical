5ef7d698ad381f02022bdf93ade92a805021ebce
omnia <omniacis1@gmail.com>
2025-06-26 00:36:00 +0300

    make listen to internet state and call set up locator

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,28 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:medical/core/api_constant.dart';
 
 import 'package:medical/features/login/presentation/view/splash_screen.dart';
+import 'package:medical/features/offline/presentaion/cubit/offline_cubit.dart';
+import 'package:medical/features/offline/presentaion/view/offline.dart';
+import 'package:medical/core/dependency_injection/service_locator.dart';
 
 import 'package:sizer/sizer.dart';
 
 void main() {
+  setupLocator();
+  ();
   runApp(
-    MyApp(), // Wrap your app
+    DevicePreview(
+      enabled: false,
+      builder: (context) =>
+          BlocProvider(create: (_) => ConnectivityCubit(), child: MyApp()),
+    ),
   );
+  Stripe.publishableKey = APIKey.stripePublishableKey;
+  WidgetsFlutterBinding.ensureInitialized();
 }
 
 class MyApp extends StatelessWidget {
@@ -17,14 +31,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Sizer(
-      builder: (context, orientation, deviceType) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-
-          home: SplashScreen(),
-        );
+    return BlocListener<ConnectivityCubit, ConnectivityStatus>(
+      listener: (context, state) {
+        if (state == ConnectivityStatus.disconnected) {
+          Navigator.of(
+            context,
+          ).pushReplacement(MaterialPageRoute(builder: (_) => OfflineScreen()));
+        }
       },
+      child: Sizer(
+        builder: (context, orientation, deviceType) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+
+            home: SplashScreen(),
+          );
+        },
+      ),
     );
   }
 }
