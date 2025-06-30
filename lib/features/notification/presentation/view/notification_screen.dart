6f72a4b499caf16175bb9098926b62d5ed93cde1
omnia <omniacis1@gmail.com>
2025-06-30 12:29:41 +0300

    edit it to show local notification

--- lib/features/notification/presentation/view/notification_screen.dart
+++ lib/features/notification/presentation/view/notification_screen.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:medical/core/constant_text.dart';
-import 'package:medical/core/dependency_injection/service_locator.dart';
 import 'package:medical/core/utils/styles.dart';
 import 'package:medical/features/home/presentation/view/home.dart';
-import 'package:medical/features/notification/presentation/cubit/notification_cubit.dart';
-import 'package:medical/features/notification/presentation/cubit/notification_state.dart';
 import 'package:medical/features/notification/data/model/notification_model.dart';
-import 'package:medical/features/notification/data/repository/notification_repository.dart';
 import 'package:medical/features/notification/presentation/view/widgets/notification_listtile.dart';
 import 'package:sizer/sizer.dart';
 
@@ -42,147 +37,59 @@ class _NotificationScreenState extends State<NotificationScreen> {
     ),
   ];
 
-  bool localCleared = false;
-
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => sl<NotificationCubit>(),
-      child: Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back_rounded),
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_rounded),
+          onPressed: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (_) => const HomeScreen()),
+            );
+          },
+        ),
+        title: Text(ConstantText.notifications, style: Styles.textStyle16),
+        actions: [
+          TextButton(
             onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => HomeScreen()),
-              );
+              setState(() {
+                notificationList.clear(); // Clear local notifications
+              });
             },
+            child: Text(ConstantText.clearAll, style: Styles.textStyle13),
           ),
-          title: Text(ConstantText.notifications, style: Styles.textStyle16),
-          actions: [
-            TextButton(
-              onPressed: () {
-                //  context.read<NotificationCubit>().clearAll();
-                setState(() {
-                  notificationList.clear(); // clear local list
-                  localCleared = true; // trigger "no notifications yet"
-                });
-              },
-              child: Text(ConstantText.clearAll, style: Styles.textStyle13),
-            ),
-          ],
-          elevation: 0,
-        ),
-        body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 5.w, vertical: 2.h),
-          child: localCleared
-              ? Center(
-                  child: Text(
-                    'No notifications yet.',
-                    style: Styles.textStyle14,
-                  ),
-                )
-              : BlocBuilder<NotificationCubit, NotificationState>(
-                  builder: (context, state) {
-                    if (state is NotificationLoading) {
-                      return const Center(child: CircularProgressIndicator());
-                    } else if (state is NotificationLoaded) {
-                      final notifications = state.notifications;
-                      if (notifications.isEmpty) {
-                        if (notificationList.isNotEmpty && !localCleared) {
-                          return ListView.builder(
-                            itemCount: notificationList.length,
-                            itemBuilder: (context, index) {
-                              final notification = notificationList[index];
-                              return Card(
-                                elevation: 0,
-                                child: NotificationListTile(
-                                  title: notification.message,
-                                  subtitle: notification.createdAt.toString(),
-                                  onTap: () {
-                                    context
-                                        .read<NotificationCubit>()
-                                        .markAsRead(notification.id);
-                                  },
-                                  onDelete: () {
-                                    context
-                                        .read<NotificationCubit>()
-                                        .deleteNotification(notification.id);
-                                  },
-                                ),
-                              );
-                            },
-                          );
-                        } else {
-                          // Show empty state
-                          return Center(
-                            child: Text(
-                              'No notifications yet.',
-                              style: Styles.textStyle14,
-                            ),
-                          );
-                        }
-                      }
-
-                      return ListView.builder(
-                        itemCount: notifications.length,
-                        itemBuilder: (context, index) {
-                          final notification = notifications[index];
-                          return Card(
-                            elevation: 0,
-                            child: NotificationListTile(
-                              title: notification.message,
-                              subtitle: notification.createdAt.toString(),
-                              onTap: () {
-                                context.read<NotificationCubit>().markAsRead(
-                                  notification.id,
-                                );
-                              },
-                              onDelete: () {
-                                context
-                                    .read<NotificationCubit>()
-                                    .deleteNotification(notification.id);
-                              },
-                            ),
-                          );
-                        },
-                      );
-                    } else if (state is NotificationError) {
-                      return ListView.builder(
-                        itemCount: notificationList.length,
-                        itemBuilder: (context, index) {
-                          final notification = notificationList[index];
-                          return Card(
-                            elevation: 0,
-                            child: NotificationListTile(
-                              title: notification.message,
-                              subtitle: notification.createdAt.toString(),
-                              onTap: () {
-                                context.read<NotificationCubit>().markAsRead(
-                                  notification.id,
-                                );
-                              },
-                              onDelete: () {
-                                context
-                                    .read<NotificationCubit>()
-                                    .deleteNotification(notification.id);
-                              },
-                            ),
-                          );
-                        },
-                      );
-                    } else {
-                      return Center(
-                        child: Text(
-                          'No notifications yet.',
-                          style: Styles.textStyle14,
-                        ),
-                      );
-                    }
-                  },
-                ),
-        ),
+        ],
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 5.w, vertical: 2.h),
+        child: notificationList.isEmpty
+            ? Center(
+                child: Text('No notifications yet.', style: Styles.textStyle14),
+              )
+            : ListView.builder(
+                itemCount: notificationList.length,
+                itemBuilder: (context, index) {
+                  final notification = notificationList[index];
+                  return Card(
+                    elevation: 0,
+                    child: NotificationListTile(
+                      title: notification.message,
+                      subtitle: notification.createdAt.toString(),
+                      onTap: () {
+                        // Optional: mark as read locally
+                      },
+                      onDelete: () {
+                        setState(() {
+                          notificationList.removeAt(index);
+                        });
+                      },
+                    ),
+                  );
+                },
+              ),
       ),
     );
   }
